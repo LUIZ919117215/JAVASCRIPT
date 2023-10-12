31dc797f695657e52a48ad07006b3d6b549a19ed
Luiz Isaque <83297869+LUIZ919117215@users.noreply.github.com>
2023-10-12 13:20:39 -0300

    a

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+let vel = 80
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel> 60) {//Condição Simple
+    console.log(`Voçê ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Digija sempre usando cinto de segurança!`)
